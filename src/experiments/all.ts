c1e751a19338902dd9fabfe41c1bdcb7feb1a7e2
Damien Clarke <dclarke@atlassian.com>
2023-05-05 16:58:44 +1000

    add pixijs hello world

--- src/experiments/all.ts
+++ src/experiments/all.ts
@@ -3,9 +3,11 @@ import type { ExperimentDefinition } from "../types";
 import blank from "./00-blank";
 import offscreenCanvas from "./01-offscreen-canvas";
 import offscreenCanvasWorker from "./02-offscreen-canvas-worker";
+import pixijsBunnies from "./03-pixijs-bunnies";
 
 export const all: ExperimentDefinition[] = [
   blank,
   offscreenCanvas,
   offscreenCanvasWorker,
+  pixijsBunnies,
 ];
