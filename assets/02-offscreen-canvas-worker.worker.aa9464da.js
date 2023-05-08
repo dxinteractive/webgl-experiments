458727349aca46c4b45b4e7ed3b0c12fd610131d
dxinteractive <dxinteractive@users.noreply.github.com>
2023-05-08 08:10:35 +0000

    deploy: 506b05797ac5d686ce14e8ffb31e2dc1d323681f

--- assets/02-offscreen-canvas-worker.worker.aa9464da.js
+++ assets/02-offscreen-canvas-worker.worker.aa9464da.js
@@ -1,1 +1,19 @@
-(function(){"use strict";const n=self;function s(){const t=new OffscreenCanvas(400,400),e=t.getContext("2d");if(!e)throw new Error("Could not create 2d offscreen canvas context");const a=Math.random()*250-125,o=Math.random()*250-125;return e.fillStyle=`lab(50% ${a} ${o})`,e.fillRect(0,0,400,400),t.transferToImageBitmap()}n.addEventListener("message",async()=>{const t=s();n.postMessage(t,[t])})})();
+(function() {
+  "use strict";
+  const _self = self;
+  function renderBitmap() {
+    const offscreen = new OffscreenCanvas(400, 400);
+    const ctx = offscreen.getContext("2d");
+    if (!ctx)
+      throw new Error("Could not create 2d offscreen canvas context");
+    const a = Math.random() * 250 - 125;
+    const b = Math.random() * 250 - 125;
+    ctx.fillStyle = `lab(50% ${a} ${b})`;
+    ctx.fillRect(0, 0, 400, 400);
+    return offscreen.transferToImageBitmap();
+  }
+  _self.addEventListener("message", async () => {
+    const bitmap = renderBitmap();
+    _self.postMessage(bitmap, [bitmap]);
+  });
+})();
